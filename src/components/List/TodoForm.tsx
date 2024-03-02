97532bac82e2a59b0b9ae9ae0d1ff72047e228fa
oweaj <qwe1520156@naver.com>
2024-03-02 16:03:26 +0900

    [Refactor] 컴포넌트 재구성 및 스타일 추가

--- src/components/List/TodoForm.tsx
+++ src/components/List/TodoForm.tsx
@@ -1,13 +1,8 @@
-import { ChangeEvent } from 'react';
 import Button from '../Common/Button';
 import useTodo from '../../hooks/useTodo';
 
 const TodoForm = () => {
   const { data, setData, handleSubmit } = useTodo();
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setData(e.target.value);
-  };
 
   return (
     <form className="flex items-center justify-between gap-8" onSubmit={handleSubmit}>
@@ -15,7 +10,7 @@ const TodoForm = () => {
         type="text"
         className="w-full p-2 border border-gray-400 rounded-md"
         placeholder="할 일을 입력해주세요."
-        onChange={handleChange}
+        onChange={(e) => setData(e.target.value)}
         value={data}
       />
       <Button type={'submit'} content={'Add'} />
